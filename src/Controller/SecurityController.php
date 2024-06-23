5126576d01bc8009ebb97258bf70f53fcc878852
Hara Ki ri <115027871+ClaudioYammi@users.noreply.github.com>
2024-06-23 10:06:46 +0300

    a

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,24 +4,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
 
 class SecurityController extends AbstractController
 {
-    /**
-     * Connexion
-     */
-<<<<<<< HEAD
-    #[Route('/', name: 'app_login')]
-=======
-    #[Route('/login', name: 'app_login')]
->>>>>>> 9c43fc10adf136ce5c6487dff5db11d01a0ec247
+    #[Route(path: '/', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
+        
         // }
 
         // get the login error if there is one
@@ -29,28 +22,16 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername, 
+            'error' => $error
+        ]);
     }
 
-    /**
-     * Déconnexion
-     */
-    #[Route('/logout', name: 'app_logout')]
+    #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
-
-    /**
-     * Accéder à son profil
-     */
-    #[Route('/profile', name: 'app_profile')]
-    public function profile(): Response
-    {
-        if ($this->getUser()) {
-            return $this->render('security/profile.html.twig');
-        } else {
-            return $this->redirectToRoute("app_login");
-        }
     }
 }
